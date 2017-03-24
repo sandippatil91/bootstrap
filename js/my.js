99975f984ef455de18acaa0d0de0261edec39278
sandip patil <sandippatil@benchmarkitsolutions.com>
2017-03-24 16:22:26 +0530

    boostrap template

---
+++ js/my.js
@@ -0,0 +1,7 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    $('.carousel').carousel({
+                    interval: 5000 //changes the speed
+                });
+                
+});
